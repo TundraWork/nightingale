e95fbeb8f614238322a094674ef159cb69612d23
Tundra Work <tundrawork@gmail.com>
2024-06-30 03:43:58 +1000

    feat: update pages

--- resources/views/rank.blade.php
+++ resources/views/rank.blade.php
@@ -89,7 +89,7 @@
                             d="M10 1a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0v-1.5A.75.75 0 0 1 10 1ZM5.05 3.05a.75.75 0 0 1 1.06 0l1.062 1.06A.75.75 0 1 1 6.11 5.173L5.05 4.11a.75.75 0 0 1 0-1.06ZM14.95 3.05a.75.75 0 0 1 0 1.06l-1.06 1.062a.75.75 0 0 1-1.062-1.061l1.061-1.06a.75.75 0 0 1 1.06 0ZM3 8a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 0 1.5h-1.5A.75.75 0 0 1 3 8ZM14 8a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 0 1.5h-1.5A.75.75 0 0 1 14 8ZM7.172 10.828a.75.75 0 0 1 0 1.061L6.11 12.95a.75.75 0 0 1-1.06-1.06l1.06-1.06a.75.75 0 0 1 1.06 0ZM10.766 7.51a.75.75 0 0 0-1.37.365l-.492 6.861a.75.75 0 0 0 1.204.65l1.043-.799.985 3.678a.75.75 0 0 0 1.45-.388l-.978-3.646 1.292.204a.75.75 0 0 0 .74-1.16l-3.874-5.764Z"
                         />
                     </svg>
-                    <span class="hidden sm:inline"><a href="/">我不是歌神 S1</a> - 选手排名</span>
+                    <a href="/"><span class="hidden sm:inline">我不是歌神 S1</span></a><span class="hidden sm:inline"> - 选手排名</span>
                 </a>
             </div>
             <!-- Left Section -->
@@ -221,11 +221,11 @@
                     />
                 </svg>
                 <span> ／ </span>
-                <span>UI -
+                <span>UI
               <a class="font-medium text-blue-400 hover:text-blue-500 hover:underline" href="https://vrchat.com/home/user/usr_0e9c75fa-ec70-4043-9fca-264c9e0af6ba">Oniya</a>
             </span>
                 <span> ／ </span>
-                <span>Backend & hosting -
+                <span>Backend & hosting
               <a class="font-medium text-blue-400 hover:text-blue-500 hover:underline" href="https://tun.cat">Tundra</a>
             </span>
             </div>
