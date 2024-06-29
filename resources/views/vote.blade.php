8842d6a8a9f3dc40fabb258d3ff61672e79f0a9b
Tundra Work <tundrawork@gmail.com>
2024-06-30 00:08:28 +1000

    feat: update vote page content

--- resources/views/vote.blade.php
+++ resources/views/vote.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
 
-    <title>观众投票系统</title>
+    <title>观众投票 - 我不是歌神 S1</title>
 
     <!-- Icons -->
     <link rel="icon" href="https://cdn.pixelcave.com/favicon.svg" sizes="any" type="image/svg+xml" />
@@ -61,7 +61,7 @@
                         <path
                             d="M10 1a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0v-1.5A.75.75 0 0 1 10 1ZM5.05 3.05a.75.75 0 0 1 1.06 0l1.062 1.06A.75.75 0 1 1 6.11 5.173L5.05 4.11a.75.75 0 0 1 0-1.06ZM14.95 3.05a.75.75 0 0 1 0 1.06l-1.06 1.062a.75.75 0 0 1-1.062-1.061l1.061-1.06a.75.75 0 0 1 1.06 0ZM3 8a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 0 1.5h-1.5A.75.75 0 0 1 3 8ZM14 8a.75.75 0 0 1 .75-.75h1.5a.75.75 0 0 1 0 1.5h-1.5A.75.75 0 0 1 14 8ZM7.172 10.828a.75.75 0 0 1 0 1.061L6.11 12.95a.75.75 0 0 1-1.06-1.06l1.06-1.06a.75.75 0 0 1 1.06 0ZM10.766 7.51a.75.75 0 0 0-1.37.365l-.492 6.861a.75.75 0 0 0 1.204.65l1.043-.799.985 3.678a.75.75 0 0 0 1.45-.388l-.978-3.646 1.292.204a.75.75 0 0 0 .74-1.16l-3.874-5.764Z" />
                     </svg>
-                    <span class="hidden sm:inline">我不是歌神 观众投票</span>
+                    <span class="hidden sm:inline">观众投票 - 我不是歌神 S1</span>
                 </a>
             </div>
             <!-- Left Section -->
@@ -137,24 +137,43 @@
     <!-- END Page Content -->
 
     <!-- Page Footer -->
-    <footer id="page-footer" class="flex flex-none items-center bg-gray-900/75">
+    <footer
+        id="page-footer"
+        class="flex flex-none items-center bg-gray-900/75"
+    >
         <div
-            class="container mx-auto flex flex-col px-4 text-center text-sm md:flex-row md:justify-between md:text-left lg:px-8 xl:max-w-6xl">
+            class="container mx-auto flex flex-col px-4 text-center text-sm md:flex-row md:justify-between md:text-left lg:px-8 xl:max-w-6xl"
+        >
             <div class="pb-1 pt-4 md:pb-4">
-                <span class="font-medium">我不是歌神</span> ©
+                &copy;
                 <script>
                     document.write(new Date().getFullYear());
                 </script>
+                <span class="font-medium">我不是歌神</span>
             </div>
-            <div class="inline-flex items-center justify-center gap-1 pb-4 pt-1 md:pt-4">
+            <div
+                class="inline-flex items-center justify-center gap-1 pb-4 pt-1 md:pt-4"
+            >
                 <span>Crafted with</span>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" data-slot="icon"
-                     class="hi-mini hi-heart inline-block size-5 text-rose-500">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    data-slot="icon"
+                    class="hi-mini hi-heart inline-block size-5 text-rose-500"
+                >
                     <path
-                        d="m9.653 16.915-.005-.003-.019-.01a20.759 20.759 0 0 1-1.162-.682 22.045 22.045 0 0 1-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 0 1 8-2.828A4.5 4.5 0 0 1 18 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 0 1-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 0 1-.69.001l-.002-.001Z" />
+                        d="m9.653 16.915-.005-.003-.019-.01a20.759 20.759 0 0 1-1.162-.682 22.045 22.045 0 0 1-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 0 1 8-2.828A4.5 4.5 0 0 1 18 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 0 1-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 0 1-.69.001l-.002-.001Z"
+                    />
                 </svg>
-                <span>by
-            <a class="font-medium text-blue-400 hover:text-blue-500 hover:underline">Oniya & YuzurihaAsano</a></span>
+                <span> / </span>
+                <span>UI -
+              <a class="font-medium text-blue-400 hover:text-blue-500 hover:underline" href="https://vrchat.com/home/user/usr_0e9c75fa-ec70-4043-9fca-264c9e0af6ba">Oniya</a>
+            </span>
+                <span> / </span>
+                <span>Backend & hosting -
+              <a class="font-medium text-blue-400 hover:text-blue-500 hover:underline" href="https://tun.cat">Tundra</a>
+            </span>
             </div>
         </div>
     </footer>
