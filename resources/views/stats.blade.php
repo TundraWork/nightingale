1eb018ec7692914a90754f0190d0a063992f6edf
Tundra Work <tundrawork@gmail.com>
2024-06-30 20:39:46 +1000

    feat: update pages

--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
 
-    <title>选手实时分数 - 我不是歌神 S1</title>
+    <title>实时分数 - 我不是歌神 S1</title>
 
     <!-- Icons -->
     <link
@@ -76,7 +76,7 @@
                     class="group inline-flex items-center gap-2 text-lg font-bold tracking-wide text-white hover:text-white/90"
                 >
                     <svg fill="#2040AA" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 364.59 364.591" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M360.655,258.05V25c0-13.807-11.191-25-25-25H130.09c-13.807,0-25,11.193-25,25v206.27 c-10.569-3.184-22.145-4.271-34.058-2.768C29.527,233.738-0.293,268.3,4.427,305.695c4.719,37.396,42.189,63.464,83.694,58.226 c40.015-5.049,66.969-37.146,66.969-73.181V50h155.564v146.794c-10.591-3.2-22.19-4.297-34.134-2.79 c-41.504,5.237-71.323,39.798-66.604,77.193s42.188,63.464,83.694,58.227C332.951,324.458,360.655,293.275,360.655,258.05z"></path> </g> </g> </g></svg>
-                    <a href="/"><span class="text-xl font-bold sm:inline">我不是歌神 S1</span></a><span class="text-l ml-2 sm:inline">选手实时分数</span>
+                    <a href="/"><span class="text-xl font-bold sm:inline">我不是歌神 S1</span></a><span class="text-l ml-2 sm:inline">实时分数</span>
                 </a>
             </div>
             <!-- Left Section -->
